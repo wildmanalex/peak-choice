dd3a4d8bbc5e5ac29d82199dd6a4df79fe75b524
Alex Wildman <Alex>
2018-01-31 16:12:24 -0700

    test4

--- skiareadata.js
+++ skiareadata.js
@@ -7,6 +7,9 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 // db.once('open', function() {
   // we're connected!
+  db.once("open", function(callback) {
+     console.log("Connection succeeded.");
+ });
 
 
 var skiAreaSchema = new mongoose.Schema({
