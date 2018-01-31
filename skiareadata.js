5459b44831690a7ae88bdabf994a36d81ef4f4c9
Alex Wildman <Alex>
2018-01-31 16:09:10 -0700

    switched port again

--- skiareadata.js
+++ skiareadata.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose')
 
 //connect to mongoose
-mongoose.connect('mongodb://localhost:27017/test')
+mongoose.connect('mongodb://localhost:27017/peakchoice')
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
